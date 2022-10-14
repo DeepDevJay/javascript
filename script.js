a735a97fc8eeb0d8507bb9ab30a5667420606b52
Jaydip <114307569+love-jayu@users.noreply.github.com>
2022-10-14 17:31:07 +0530

    day 4 assignment added

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-console.log("hello")
 
-let num1 = 10;
-let num2 = 20;
 
-if (num1 > num2)
+var x, y, num="Hello";
+
+for(x=1; x <=9; x++)
 {
-    console.log(num1,"is greater number")
-}
-else
-{
-    console.log(num2,"is greater number")
+   for (y=1; y < x; y++)
+    {
+        num=num+(y+" ");        
+    }
+ console.log(num);
+ num='';    
 }
