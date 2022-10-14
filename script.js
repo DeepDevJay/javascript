8c23225e6936ed0942ed9d6547cf9a233b63bcdf
Jaydip <114307569+love-jayu@users.noreply.github.com>
2022-10-14 17:35:59 +0530

    day 4 assignment is added with day 3 (combined)

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+console.log("hello")
 
+let num1 = 10;
+let num2 = 20;
+
+if (num1 > num2)
+{
+    console.log(num1,"is greater number")
+}
+else
+{
+    console.log(num2,"is greater number")
+}
+
+
+// Day 4 Assignment
 
 var x, y, num="Hello";
 
